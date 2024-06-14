2bc65ff8b85b157db0d56f71744aa05ba78eeaf9
HUANG-TZU-HUAN <125246658+HUANG-TZU-HUAN@users.noreply.github.com>
2024-06-14 12:35:42 +0800

    ai

--- app.py
+++ app.py
@@ -50,3 +50,55 @@ def get_gemini_response(query):
     return f"Gemini API 回應: {response.text}"
 if __name__ == "__main__":
     app.run(port=5000)
+
+genai.configure(api_key=os.environ["AIzaSyC0xHIJ922U7TofueVkuv2QwOp_pjId1Jc"])
+
+# Create the model
+# See https://ai.google.dev/api/python/google/generativeai/GenerativeModel
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+  generation_config=generation_config,
+  # safety_settings = Adjust safety settings
+  # See https://ai.google.dev/gemini-api/docs/safety-settings
+)
+
+response = model.generate_content([
+  "input: 我想要預約掛號。",
+  "output: 好的 請問哪裡不舒服？",
+  "input: 我牙齒痛",
+  "output: 根據的您的情況建議您掛牙科，請問需要幫您預約嗎？",
+  "input: 好",
+  "output: (提示用戶輸入)掛號時間、科別、醫師、就醫時段",
+  "input: 輸入完成",
+  "output: 已預約成功",
+  "input: 我想要取消預約",
+  "output: 好的(提示用戶輸入)掛號時間、科別、醫師、就醫時段",
+  "input: 輸入完成",
+  "output: 已取消預約成功",
+  "input: 我想要查詢預約",
+  "output: 好的(提示用戶輸入)查詢日期、科別、醫師、就醫時段",
+  "input: 輸入完成",
+  "output: 顯示結果",
+  "input: 我想要查詢看診進度",
+  "output: 好的(提示用戶輸入)科別、醫師、就醫時段",
+  "input: 輸入完成",
+  "output: 顯示結果",
+  "input: 我想要查看醫師的專長 像是有關牙科的醫生",
+  "output: 這方面駱醫師是專業推薦給您，需要幫您預約嗎？",
+  "input: 好",
+  "output: .....回到預約功能",
+  "input: 你是誰",
+  "output: 我是一名診所的服務機器人。可以幫您預約掛號、取消預約、查詢預約、查詢看診進度、以及查看醫師專長。請問您今天需要我做些什麼呢？",
+  "input: 你是誰",
+  "output: ",
+])
+
+print(response.text)
